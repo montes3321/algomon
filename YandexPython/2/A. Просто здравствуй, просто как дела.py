b9517b129352146b612443073b703e90408021f0
AlexVatolin <332montes332@gmail.com>
2024-02-04 01:08:09 +0300

    Yandex2A - UPD

--- YandexPython/2/A. Просто здравствуй, просто как дела.py
+++ YandexPython/2/A. Просто здравствуй, просто как дела.py
@@ -1,1 +1,9 @@
 # https://new.contest.yandex.ru/41234/problem?id=149944/2022_10_12/ZP5AL2TjmP
+
+name = input("Как Вас зовут?\n")
+print(f"Здравствуйте, {name}!")
+answer = input("Как дела?\n")
+if answer == "хорошо":
+    print("Я за вас рада!")
+if answer == "плохо":
+    print("Всё наладится!")
