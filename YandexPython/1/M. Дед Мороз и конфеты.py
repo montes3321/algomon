8b864fffd00d9b39b0f10c9f93585aa32645368b
AlexVatolin <332montes332@gmail.com>
2024-02-03 10:44:19 +0300

    Yandex1M UPD

---
+++ YandexPython/1/M. Дед Мороз и конфеты.py
@@ -0,0 +1,7 @@
+# https://new.contest.yandex.ru/41233/problem?id=149944/2022_10_11/IUHp8TlSfb
+
+monsters = int(input())
+cookies = int(input())
+
+print(cookies // monsters)
+print(cookies % monsters)
