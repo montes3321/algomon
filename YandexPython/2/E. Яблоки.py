6b73dd56bba1e8ed7c45c2b515f45c03cbbc27e0
AlexVatolin <332montes332@gmail.com>
2024-02-04 12:10:59 +0300

    Yandex2E - UPD

---
+++ YandexPython/2/E. Яблоки.py
@@ -0,0 +1,10 @@
+# https://new.contest.yandex.ru/41234/problem?id=149944/2022_10_12/N16SQhB7I5
+
+# pet 7 vas 6
+# pet 6 + N vas 12 + M
+N = int(input())
+M = int(input())
+if N + 6 > 12 + M:
+    print("Петя")
+else:
+    print("Вася")
